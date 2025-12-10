9505e32f48390974e25cb390c07c7f88d55e3769
Febrianando <mohammadfebrianando.23124@mhs.unesa.ac.id>
2025-12-11 03:45:03 +0700

    fix api handler

--- src/app.js
+++ src/app.js
@@ -7,13 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Endpoint test untuk Vercel
+// TEST ROUTE
 app.get('/api', (req, res) => {
-  res.json({ message: 'TravSecure API from Vercel is running' });
+  res.json({ message: 'TravSecure API is running on Vercel!' });
 });
-
-// TODO: di bawah sini nanti kamu pasang semua routes asli kamu
-// app.use('/api/auth', authRoutes);
-// app.use('/api/users', userRoutes);
 
 module.exports = app;
