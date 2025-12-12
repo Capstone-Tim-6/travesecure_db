fce1e87dde97fa027006734a86d8b5bf130284f7
Febrianando <mohammadfebrianando.23124@mhs.unesa.ac.id>
2025-12-12 11:05:12 +0700

    update api dan index

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
-require('dotenv').config();
-const app = require('./src/app');
+// api/index.js
+const app = require('../src/app');
 
-// Jangan pakai app.listen!
-
-module.exports = app;
+// Vercel butuh 1 fungsi sebagai export utama
+module.exports = (req, res) => {
+  return app(req, res);
+};
+export default function handler(req, res) {
+  res.status(200).json({ message: "API Vercel Route Works!" });
+}
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-console.log("INDEX.JS MULAI DIJALANKAN..."); // debug
-
 require('dotenv').config();
 const app = require('./src/app');
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`SERVER BERHASIL JALAN DI PORT ${port}`);
+  console.log(`Local server running on port ${port}`);
 });
