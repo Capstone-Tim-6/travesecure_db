02fb6776adc05d7f24b10841b4d6be5fff5926e5
Febrianando <149350897+Febrianando@users.noreply.github.com>
2025-12-12 15:17:38 +0700

    index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,2 @@
-// api/index.js
 const app = require('../src/app');
-
-// Vercel butuh 1 fungsi sebagai export utama
-module.exports = (req, res) => {
-  return app(req, res);
-};
-export default function handler(req, res) {
-  res.status(200).json({ message: "API Vercel Route Works!" });
-}
+module.exports = (req, res) => app(req, res);
